4960bc5f2bfc9e35c567df6a10e0e009f8e6d7a2
Klaudia <klaudia.krzywda@gmail.com>
2019-11-28 19:17:52 +0000

    Updates

---
+++ precache-manifest.76ae03b1b95fe487d494b8011ffedb97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eca90de3f76b961409bee459e5282de",
+    "url": "/adludio/index.html"
+  },
+  {
+    "revision": "94c3562bee4b1881ad53",
+    "url": "/adludio/static/css/main.b9d5284b.chunk.css"
+  },
+  {
+    "revision": "5f4f45ef0d5e2b04fe97",
+    "url": "/adludio/static/js/2.38e558eb.chunk.js"
+  },
+  {
+    "revision": "94c3562bee4b1881ad53",
+    "url": "/adludio/static/js/main.8f76d9ab.chunk.js"
+  },
+  {
+    "revision": "4c5a288c312cef5048d8",
+    "url": "/adludio/static/js/runtime-main.a68b7b3d.js"
+  },
+  {
+    "revision": "aa6ffae2de9588343be7d5b990495ac3",
+    "url": "/adludio/static/media/asset1.aa6ffae2.jpg"
+  },
+  {
+    "revision": "585afc787e1475b6bb6ff30887580c53",
+    "url": "/adludio/static/media/asset2.585afc78.png"
+  }
+]);
